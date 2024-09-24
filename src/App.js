b651ddfb0168ea72f7759a601f703fc9b46856ea
Eli Ugolino <eli.ugolino@gmail.com>
2024-09-24 14:44:17 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import Counter from './Counter'
-import Todo from './Todo'
-import './App.css';
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './features/counterSlice'
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-      <Todo />
-    </div>
-  );
+function Counter() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+    const [input, setInput] = useState(0)
+
+    const byAmount = (e) => {
+        e.preventDefault()
+        dispatch(incrementByAmount(Number(input)))
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}>
+                Increment
+            </button>
+            <button
+                aria-label="Decrement value"
+                onClick={() => dispatch(decrement())}>
+                Decrement
+            </button>
+            <form onSubmit={(e) => byAmount(e)}>
+                <input type="number" onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
-export default App;
+export default Counter
